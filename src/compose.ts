03e653bb108dfc1ddcda67811c629ed01fa92b38
alex <alex.karamushko@nordclan.com>
2021-09-09 13:44:44 +0300

    add composition laws example

--- src/compose.ts
+++ src/compose.ts
@@ -1,24 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /**
  * Композиция (right-to-left)
  */
 
-import { compose, identity } from './utils';
+import { compose, curry, equals } from 'rambda';
+import { identity } from './utils';
 
 const data = ['james', '', 'jack', 'max', '', 'alex'];
+
+// Убираем явное оперирование данными (pointfree)
+const filter = curry((fn: Function, array: any) => array.filter(fn));
+const map = curry((fn: Function, array: any) => array.map(fn));
 
 function uppercaseFirstLetter(str: string): string {
   return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
 
-function uppercaseNames(names: string[]): string[] {
-  return names.map(uppercaseFirstLetter);
+function addExclamation(str: string): string {
+  return `${str}!`;
 }
 
-function filterEmptyNames(names: string[]): string[] {
-  return names.filter(identity);
-}
+// function uppercaseNames(names: string[]): string[] {
+//   return names.map(uppercaseFirstLetter);
+// }
 
-const processNames = compose(uppercaseNames, filterEmptyNames);
+// function filterEmptyNames(names: string[]): string[] {
+//   return names.filter(identity);
+// }
 
-// eslint-disable-next-line no-console
-console.log(processNames(data));
+// Коммутативность
+// const processNames1 = compose(map(uppercaseFirstLetter), map(addExclamation), filter(identity));
+// const processNames2 = compose(map(addExclamation), filter(identity), map(uppercaseFirstLetter));
+// // // eslint-disable-next-line no-console
+// console.log(equals(processNames1(data), processNames2(data)));
+
+// Ассоциативность
+// const filterOutAndExclaim = compose(map(addExclamation), filter(identity));
+// const processNames1 = compose(map(uppercaseFirstLetter), filterOutAndExclaim);
+// const processNames2 = compose(filterOutAndExclaim, map(uppercaseFirstLetter));
+// // eslint-disable-next-line no-console
+// console.log(equals(processNames1(data), processNames2(data)));
+
+// Дистрибутивность
+// const exclaimAndUppercase = compose(uppercaseFirstLetter, addExclamation);
+// const processNames1 = compose(map(exclaimAndUppercase), filter(identity));
+// // eslint-disable-next-line no-console
+// console.log(equals(processNames1(data), processNames2(data)));
+
+// Тоджественность
+// const exclaimAndUppercase = compose(uppercaseFirstLetter, addExclamation);
+// const processNames1 = compose(identity, map(exclaimAndUppercase));
+// const processNames2 = compose(map(exclaimAndUppercase), identity);
+
+// // eslint-disable-next-line no-console
+// console.log(equals(processNames1(data), processNames2(data)));
+// // eslint-disable-next-line no-console
+// console.log(equals(processNames1(data), map(exclaimAndUppercase)(data)));
+// // eslint-disable-next-line no-console
+// console.log(equals(processNames2(data), map(exclaimAndUppercase)(data)));
